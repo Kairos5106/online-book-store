750ee6849f182ea60d32a9dfa039c8ed05b4589d
kevinhotzewan <kevinhotzewan@gmail.com>
2024-06-09 18:49:34 +0800

    integrated an admin template into website

--- book-haven/resources/views/admin/home.blade.php
+++ book-haven/resources/views/admin/home.blade.php
@@ -1,15 +1,18 @@
-<x-app-layout>
-
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Test
-</body>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      @include('admin.sidebar')
+      <div class="container-fluid page-body-wrapper">
+        @include('admin.header')
+        @include('admin.body')
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include('admin.script')
+  </body>
 </html>
