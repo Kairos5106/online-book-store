eb9fe484decb9bdde92125f8f0eb70d5c8f9eb4b
unknown <minen10744@gmail.com>
2024-06-10 14:26:31 +0800

    completed customer page features: add to cart and payment functionality

--- book-haven/routes/web.php
+++ book-haven/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,28 +15,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);  
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/book_categories', [AdminController::class, 'view_category']);
+
+Route::post('/add_category', [AdminController::class, 'add_category']);
+
+Route::get('/delete_category/{id}', [AdminController::class, 'delete_category']);
+
+Route::get('/view_product_form', [AdminController::class, 'view_product_form']);
+
+Route::post('/add_product', [AdminController::class, 'add_product']);
+
+Route::get('/show_product', [AdminController::class, 'show_product']);
+
+Route::get('/update_product/{id}', [AdminController::class, 'update_product']);
+
+Route::post('/edit_product/{id}', [AdminController::class, 'edit_product']);
+
+Route::get('/delete_product/{id}', [AdminController::class, 'delete_product']);
+
 Route::get('/redirect', [HomeController::class, 'redirect']);
 
-Route::get('/book_categories', [AdminController::class, 'view_category']);  
+route::get('/product_details/{id}',[HomeController::class,'product_details']);
 
-Route::post('/add_category', [AdminController::class, 'add_category']);  
+route::post('/add_cart/{id}',[HomeController::class,'add_cart']);
 
-Route::get('/delete_category/{id}', [AdminController::class, 'delete_category']);  
+route::get('/show_cart',[HomeController::class,'show_cart']);
 
-Route::get('/view_product_form', [AdminController::class, 'view_product_form']);  
+route::get('/remove_cart/{id}',[HomeController::class,'remove_cart']);
 
-Route::post('/add_product', [AdminController::class, 'add_product']);  
+route::get('/cash_order',[HomeController::class,'cash_order']);
 
-Route::get('/show_product', [AdminController::class, 'show_product']);  
+route::get('/stripe/{totalprice}',[HomeController::class,'stripe']);
 
-Route::get('/update_product/{id}', [AdminController::class, 'update_product']);  
+Route::post('stripe/{totalprice}', [HomeController::class,'stripePost'])->name('stripe.post');
 
-Route::post('/edit_product/{id}', [AdminController::class, 'edit_product']);  
+route::get('/show_order',[HomeController::class,'show_order']);
 
-Route::get('/delete_product/{id}', [AdminController::class, 'delete_product']);  
+route::get('/cancel_order/{id}',[HomeController::class,'cancel_order']);
